ad96005b0ace2506f0365b98aa7994680c07ec21
Cloud Dark <clouddark1798@gmail.com>
2025-11-24 10:50:38 +0700

    chore: update example.js to use English language

--- example/example.js
+++ example/example.js
@@ -16,26 +16,26 @@ const end = {
 const startString = "30.849635,-83.24559";
 const endString = "27.950575,-82.457178";
 
-console.log("Jarak antara dua titik (km):", haversine(start, end));
-console.log("Jarak antara dua titik (mil):", haversine(start, end, { unit: 'mile' }));
-console.log("Jarak antara dua titik (meter):", haversine(start, end, { unit: 'meter' }));
-console.log("Jarak antara dua titik (cm):", haversine(start, end, { unit: 'cm' }));
-console.log("Jarak antara dua titik (ft):", haversine(start, end, { unit: 'ft' }));
-console.log("Jarak antara dua titik (inci):", haversine(start, end, { unit: 'in' }));
-console.log("Apakah jarak < 1 km? (true/false):", haversineModule.haversineIsWithin(start, end, 1));
-console.log("Apakah jarak < 1 mil? (true/false):", haversineModule.haversineIsWithin(start, end, 1, { unit: 'mile' }));
-console.log("Apakah jarak < 1 meter? (true/false):", haversineModule.haversineIsWithin(start, end, 1, { unit: 'meter' }));
+console.log("Distance between two points (km):", haversine(start, end));
+console.log("Distance between two points (miles):", haversine(start, end, { unit: 'mile' }));
+console.log("Distance between two points (meters):", haversine(start, end, { unit: 'meter' }));
+console.log("Distance between two points (cm):", haversine(start, end, { unit: 'cm' }));
+console.log("Distance between two points (ft):", haversine(start, end, { unit: 'ft' }));
+console.log("Distance between two points (in):", haversine(start, end, { unit: 'in' }));
+console.log("Is distance < 500 km? (true/false):", haversineModule.haversineIsWithin(start, end, 500));
+console.log("Is distance < 500 miles? (true/false):", haversineModule.haversineIsWithin(start, end, 500, { unit: 'mile' }));
+console.log("Is distance < 500 meters? (true/false):", haversineModule.haversineIsWithin(start, end, 500, { unit: 'meter' }));
 
 // Using string coordinates:
-console.log("Jarak dengan string coordinates:", haversine(startString, endString));
+console.log("Distance with string coordinates:", haversine(startString, endString));
 
 // Using the new functions:
 const { haversineBearing, haversineMidpoint } = haversineModule;
 
-console.log("Arah antara dua titik (derajat dari utara):", haversineBearing(start, end));
-console.log("Titik tengah antara dua titik:", haversineMidpoint(start, end));
+console.log("Bearing between two points (degrees from North):", haversineBearing(start, end));
+console.log("Midpoint between two points:", haversineMidpoint(start, end));
 
 // Using additional units:
-console.log("Jarak dalam cm:", haversine(start, end, { unit: 'cm' }));
-console.log("Jarak dalam ft:", haversine(start, end, { unit: 'ft' }));
-console.log("Jarak dalam inci:", haversine(start, end, { unit: 'in' }));
+console.log("Distance in cm:", haversine(start, end, { unit: 'cm' }));
+console.log("Distance in ft:", haversine(start, end, { unit: 'ft' }));
+console.log("Distance in inches:", haversine(start, end, { unit: 'in' }));
